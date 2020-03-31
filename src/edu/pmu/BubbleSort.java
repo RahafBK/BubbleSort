a3b869c7e70b96c53fe73d047a4436d873a8948e
Mariam-2000 <62655495+Mariam-2000@users.noreply.github.com>
2020-03-31 21:43:16 +0300

    Bubble sort code is now improved

--- src/edu/pmu/BubbleSort.java
+++ src/edu/pmu/BubbleSort.java
@@ -20,13 +20,19 @@ public class BubbleSort {
     }
 
     static void BubbleSort(int arr[]){
+        int temp;
+        Boolean swap;
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
+            swap = false;
             for (int j = 0; j < n-i-1; j++)
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swap = true;
                 }
+        if (swap==false)
+            break;
     }
 }
